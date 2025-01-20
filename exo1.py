5e29c6b45236a29703989207d8e333d529ff09cc
YassineBouchbika <yassine.bouchbika@polymtl.ca>
2025-01-20 15:05:42 -0500

    Exo 1

--- exo1.py
+++ exo1.py
@@ -1,8 +1,14 @@
 # Demander le nom complet de l'utilisateur
+nom_complet = input("Veuillez entrer votre nom complet: ")
 # Demander l'âge de l'utilisateur
+âge = int(input("Veuillez entrez votre âge: "))
 
 # Définir l'année actuelle
+année_actuelle = 2025
 # Calculer l'année de naissance
+année_de_naissance = année_actuelle-âge
 
 # Afficher un message de bienvenue avec le nom complet
+print("Bonjour", nom_complet)
 # Afficher l'année de naissance
+print(année_de_naissance)
