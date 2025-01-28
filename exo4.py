4d4989d8a52289114c94935d65f6b7b7b3c2917a
JuniorTeck <juniorteckent@outlook.com>
2025-01-28 17:02:52 -0500

    tout les tests passent

--- exo4.py
+++ exo4.py
@@ -6,19 +6,23 @@ annees = semaines = jours = heures = minutes = 0
 
 # TODO: Assigner à la variable "annees" le nombre d'années
 annees = math.floor(secondes / (60*60*24*365))
-resteDeSeconde = (secondes % (60*60*24*365))
-print("le reste est: ", resteDeSeconde)
 # TODO: Assigner à la variable "semaines" le nombre de semaines restantes
-semaines = math.floor(resteDeSeconde/ (60*60*24*7))
 resteDeSeconde = (secondes % (60*60*24*365))
+semaines = math.floor(resteDeSeconde / (60*60*24*7))
 # TODO: Assigner à la variable "jours" le nombre de jours restants
-jours = math.floor(secondes % ((60*60*24*7) / (60*60*24*7)))
+resteDeSeconde = resteDeSeconde % (60*60*24*7)
+jours = math.floor(resteDeSeconde / (60*60*24))
 # TODO: Assigner à la variable "heures" le nombre d'heures restantes
-
+resteDeSeconde = resteDeSeconde % (60*60*24)
+heures = math.floor(resteDeSeconde / (60*60))
 # TODO: Assigner à la variable "minutes" le nombre de minutes restantes
-
+resteDeSeconde = resteDeSeconde % (60*60)
+minutes = math.floor(resteDeSeconde / (60))
 # TODO: Assigner à la variable "secondes" le nombre de secondes restantes
-
+resteDeSeconde = resteDeSeconde % (60)
+secondes = math.floor(resteDeSeconde)
 # TODO: Afficher le nombres d'années, semaines, jours, heures, minutes et secondes
 # Exemple: print(annees, semaines, jours, heures, minutes, secondes)
-print(str(annees) + " an(s), " + str(semaines) + " sem(s), " + str(jours) + " jour(s), " )
+print(annees, semaines, jours, heures, minutes, secondes)
+
+#print(str(annees) + " an(s), " + str(semaines) + " sem(s), " + str(jours) + " jour(s), " + str(heures) + " heure(s), " + str(minutes) + " minute(s), " + str(secondes) + " secondes(s), " )
